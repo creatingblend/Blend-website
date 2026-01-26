611f0eb2943d985c7a72fa2f4c665e1c7dcca550
TanujKumar41 <tanujkumar191001@gmail.com>
2026-01-26 23:05:50 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -54,8 +54,8 @@
       target: 'esnext',
       outDir: 'dist',
     },
-    server: {
-      port: 3000,
-      open: true,
-    },
+    // server: {
+    //   port: 3000,
+    //   open: true,
+    // },
   });
